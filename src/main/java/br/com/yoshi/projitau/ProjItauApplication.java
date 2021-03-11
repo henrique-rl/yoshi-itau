5fdf68c042782644e9704509da53105c5a428b3e
arms <arms_1991@hotmail.com>
2021-03-11 09:55:00 -0300

    add projeto

---
+++ src/main/java/br/com/yoshi/projitau/ProjItauApplication.java
@@ -0,0 +1,13 @@
+package br.com.yoshi.projitau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjItauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjItauApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/yoshi/projitau/ProjItauApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.yoshi.projitau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjItauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
